6d029a2b4b7a0b798cafe8aa4c1448f9ee18f853
Swxpb1863 <113461836+Swxpb1863@users.noreply.github.com>
2024-09-03 13:48:47 +0530

    Add files via upload

---
+++ Hexaware_Hackathon/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
